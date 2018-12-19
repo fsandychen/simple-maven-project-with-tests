8a2689e5e8e2b22e4e54cbc66cbc0419a7800339
Andy Chen <fs.andychen@gmail.com>
2018-12-19 18:37:54 +0800

    update

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -32,7 +32,7 @@ class Base {
     protected void run() {
         double r = Math.random();
         if (r < 0.1) {
-            fail("Hello!!!!!!!");
+            fail("Hello World!!!!!!!");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
